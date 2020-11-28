8ee5e16864b81ff20e099a5849a69ec246e206ac
NIL-zhuang <181830266@smail.nju.edu.cn>
2020-11-28 18:35:10 +0800

    wordcount生成json

--- NewsAnalysis/wordcount.py
+++ NewsAnalysis/wordcount.py
@@ -5,12 +5,12 @@ from pyspark.streaming import StreamingContext
 
 class Cluster:
     def __init__(self):
-        self.spark = SparkSession.builder.appName("WordCount").getOrCreate()
+        self.spark = SparkSession.builder.appName("WordCount").master("local[*]").getOrCreate()
         self.sc = self.spark.sparkContext
         self.ssc = StreamingContext(self.sc, 20)
         self.out_stream = self.ssc.textFileStream("hdfs://mark-pc:9000/data")
         word_count = self.out_stream.flatMap(lambda line: line.split(' ')).map(
-            lambda x: (x, 1)).reduceByKey(lambda x, y: x+y).repartition(1).saveAsTextFiles("./word")
+            lambda x: (x, 1)).reduceByKey(lambda x, y: x+y).repartition(1).saveAsTextFiles("./wct/word")
 
 
 if __name__ == "__main__":
---
+++ wct_handle.py
@@ -0,0 +1,51 @@
+import os
+import json
+import time
+import shutil
+
+
+def isDigit(word: str):
+    try:
+        return isinstance(int(word), int)
+    except ValueError:
+        return False
+
+
+def get_count(filename):
+    with open(filename) as f:
+        lines = f.readlines()
+        if len(lines) == 0:
+            return None
+    words = [eval(line) for line in lines]
+    words = [word for word in words if not isDigit(word[0])]
+    words = sorted(words, key=lambda x: x[1], reverse=True)[:20]
+    res = list()
+    for word in words:
+        t = dict()
+        t["关键词"] = word[0]
+        t["数量"] = word[1]
+        res.append(t)
+    return res
+
+
+if __name__ == "__main__":
+    # 小时，词，关键词
+    item_list = []
+    cur_time = time.strftime("%Y-%m-%d %H:%M", time.localtime())
+    for dirs in os.listdir("./wct"):
+        data = get_count("./wct/"+dirs+"/part-00000")
+        if data is None:
+            shutil.rmtree("./wct/"+dirs)
+            continue
+        cur_json = {cur_time: data}
+        print(cur_json)
+        fp = open("./wordcount_history.json")
+        model = json.load(fp)
+        fp.close()
+        for i in cur_json:
+            model[i] = cur_json[i]
+        jsObj = json.dumps(model, ensure_ascii=False)
+        with open("./wordcount_history.json", "w", encoding='utf-8') as fw:
+            fw.write(jsObj)
+            fw.close()
+        shutil.rmtree("./wct/"+dirs)
